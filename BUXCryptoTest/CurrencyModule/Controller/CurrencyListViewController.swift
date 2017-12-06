e9682a7265124dfe4cd20be9c835a4f781a88c28
Mykola Hrybeniuk <gkolunia@gmail.comm>
2017-12-06 20:35:27 +0200

    add - presenter for currency list and define protocol of presenter in list controller.

--- BUXCryptoTest/CurrencyModule/Controller/CurrencyListViewController.swift
+++ BUXCryptoTest/CurrencyModule/Controller/CurrencyListViewController.swift
@@ -9,6 +9,18 @@
 import Foundation
 import UIKit
 
+/// Interface of class which prepares list of available currencies.
+protocol CurrencyAvailableListPreparing {
+    
+    /// Make loading of users list.
+    func loadCurrencyList()
+    
+    /// Do action on selecting one of users from list.
+    func doOnSelect(_ userViewModel: CurrencyViewModel)
+}
+
 class CurrencyListViewController : UIViewController {
+    
+    var presenter : CurrencyAvailableListPreparing!
     
 }
