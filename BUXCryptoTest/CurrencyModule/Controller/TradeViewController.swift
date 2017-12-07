8616e55ec13aae03a0d718c82e0ffba942ff605d
Mykola Hrybeniuk <gkolunia@gmail.com>
2017-12-08 00:02:29 +0200

    implement - trade view.

--- BUXCryptoTest/CurrencyModule/Controller/TradeViewController.swift
+++ BUXCryptoTest/CurrencyModule/Controller/TradeViewController.swift
@@ -11,21 +11,53 @@ import UIKit
 
 protocol TradingOperator {
     var viewModel : CurrencyViewModel { get }
-    func doBuying(_ amount: Int)
-    func doSelling(_ amount: Int)
+    func doBuying(_ amount: String)
+    func doSelling(_ amount: String)
+    func currencyInputFormatting(_ inputString: String) -> String
 }
 
 class TradeViewController : BaseViewController, ErrorPresentable {
 
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var sellButton: UIButton!
+    
     var presenter : TradingOperator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Trade"
+        amountTextField.addTarget(self, action: #selector(myTextFieldDidChange), for: .editingChanged)
+        buyButton.isEnabled = false
+        sellButton.isEnabled = false
     }
     
-    func show(_ errorViewModel: ErrorViewModel) {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        amountTextField.becomeFirstResponder()
+    }
     
+    @objc func myTextFieldDidChange(_ textField: UITextField) {
+        let formatedString = presenter.currencyInputFormatting(textField.text!)
+        textField.text = formatedString
+        buyButton.isEnabled = formatedString.count > 0
+        sellButton.isEnabled = formatedString.count > 0
+    }
+    
+    //MARK: ErrorPresentable
+    func show(_ errorViewModel: ErrorViewModel) {
+        
+    }
+    
+    //MARK: IBActions
+    @IBAction func onSellClicked(_ sender: Any) {
+        presenter.doBuying(amountTextField.text!)
+    }
+    
+    @IBAction func onBuyClicked(_ sender: Any) {
+        presenter.doSelling(amountTextField.text!)
     }
     
 }
+
+
--- BUXCryptoTest/CurrencyModule/Presenter/TradingPresenter.swift
+++ BUXCryptoTest/CurrencyModule/Presenter/TradingPresenter.swift
@@ -22,12 +22,37 @@ class TradingPresenter: TradingOperator {
         showingView = defaultShowingView
     }
     
-    func doBuying(_ amount: Int) {
+    func doBuying(_ amount: String) {
         
     }
     
-    func doSelling(_ amount: Int) {
+    func doSelling(_ amount: String) {
         
+    }
+    
+    func currencyInputFormatting(_ inputString: String) -> String {
+        var number: NSNumber!
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currencyAccounting
+        formatter.currencySymbol = "$"
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        
+        var amountWithPrefix = inputString
+        
+        // remove from String: "$", ".", ","
+        let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+        amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, inputString.count), withTemplate: "")
+        
+        let double = (amountWithPrefix as NSString).doubleValue
+        number = NSNumber(value: (double / 100))
+        
+        // if first number is 0 or all numbers were deleted
+        guard number != 0 as NSNumber else {
+            return ""
+        }
+        
+        return formatter.string(from: number)!
     }
     
 }
